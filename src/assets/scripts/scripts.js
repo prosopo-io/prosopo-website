61bf331d021fd67887df2ecefd06df3351cf0cbc
Lupus7 <sinisa.canak997@gmail.com>
2022-07-25 14:41:34 +0200

    add email form

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -4,7 +4,6 @@
 (function($) {
     "use strict";
 	$(document).ready(function() {
-
 		/* Navbar Scripts */
 		// jQuery to collapse the navbar on scroll
 		$(window).on('scroll load', function () {
@@ -21,6 +20,54 @@
 			$('.offcanvas-collapse').toggleClass('open')
 		})
 
+		function showElem(id) {
+            document.getElementById(id).style["visibility"] = "visible";
+            document.getElementById(id).style["position"] = "relative";
+        }
+
+        function hideElem(id) {
+            document.getElementById(id).style["visibility"] = "hidden";
+            document.getElementById(id).style["position"] = "absolute";
+        }
+
+        hideElem("subscribe-loader");
+        hideElem("subscribe-success");
+
+        function setEmailError(err) {
+            document.getElementById("subscribe-error").textContent = err;
+        }
+
+        window.onEmailFormSubmit = async (event) => {
+            event.preventDefault();
+            showElem("subscribe-loader");
+            try {
+                window.loader = true;
+                const formData = new FormData(event.target);
+                const formProps = Object.fromEntries(formData);
+                const res = await fetch("/.netlify/functions/subscribe", {
+                    body: JSON.stringify(formProps),
+                    method: "POST",
+                });
+
+				// 499 - set as Mongo error on backend
+                if (res.status === 404 || res.status === 499) {
+                    throw new Error(
+                        "Something went wrong! Please try again later!"
+                    );
+                }
+
+                const json = await res.json();
+                if (res.status != 200) {
+                    throw new Error(json.message);
+                }
+
+                hideElem("subscribe-form");
+                showElem("subscribe-success");
+            } catch (err) {
+                setEmailError(err.message);
+            }
+            hideElem("subscribe-loader");
+        };
 	});
 
 })(jQuery);
